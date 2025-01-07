e5f0c87f07c781884fa38614643d19a92de065ef
Kimseonkyoung <kwswaver@gmail.com>
2025-01-08 00:48:30 +0900

    chore: 패키지 구조 및 클래스파일 수정, 삭제

--- src/main/java/kr/hhplus/be/server/application/service/TokenService.java
+++
@@ -1,4 +0,0 @@
-package kr.hhplus.be.server.application.service;
-
-public class TokenService {
-}
--- src/main/java/kr/hhplus/be/server/interfaces/api/dto/BalanceChargeRequest.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.interfaces.api.dto;
-
-import lombok.Data;
-
-@Data
-public class BalanceChargeRequest {
-    private Long userId;  // 사용자 ID
-    private Integer amount; // 충전 금액
-}

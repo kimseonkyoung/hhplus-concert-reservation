411db204bda142cd4aad2d994e439fc60ba69bab
Kimseonkyoung <kwswaver@gmail.com>
2025-01-08 00:53:00 +0900

    feat: 공통 글로벌 핸들러 생성 및 공통 에러 코드 응답 생성

--- src/main/java/kr/hhplus/be/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/hhplus/be/server/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,32 @@
 package kr.hhplus.be.server.common.exception;
 
-public class GlobalExceptionHandler {
+
+import kr.hhplus.be.server.domain.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static kr.hhplus.be.server.common.exception.ErrorCode.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> HandlerException(Exception e) {
+        return ResponseEntity.status(500).body(new kr.hhplus.be.server.common.exception.ErrorResponse(COMMON_ERROR));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(BAD_REQUEST));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> UserNotFoundException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(MISSING_USER));
+    }
+
+
+
 }

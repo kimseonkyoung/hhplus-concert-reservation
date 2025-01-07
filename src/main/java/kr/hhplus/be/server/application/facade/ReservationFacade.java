08ff55168f82ea6c9230dcfa8e05192ec933ec2d
Kimseonkyoung <kwswaver@gmail.com>
2025-01-08 00:58:07 +0900

    feat: service를 조립하기 위한 facade 생성 및 DTO 컨버터 생성
    - Convert: controller dto <-> service dto

--- src/main/java/kr/hhplus/be/server/application/facade/ReservationFacade.java
+++ src/main/java/kr/hhplus/be/server/application/facade/ReservationFacade.java
@@ -1,4 +1,63 @@
 package kr.hhplus.be.server.application.facade;
 
+import kr.hhplus.be.server.application.mapper.facade.TokenDtoConverter;
+import kr.hhplus.be.server.application.mapper.facade.UserDtoConverter;
+import kr.hhplus.be.server.domain.token.TokenService;
+import kr.hhplus.be.server.domain.User.UserService;
+import kr.hhplus.be.server.domain.service.dto.TokenServiceResponse;
+import kr.hhplus.be.server.domain.service.dto.UserServiceRequest;
+import kr.hhplus.be.server.domain.service.dto.UserServiceResponse;
+import kr.hhplus.be.server.interfaces.api.dto.BalanceRequest;
+import kr.hhplus.be.server.interfaces.api.dto.BalanceResponse;
+import kr.hhplus.be.server.interfaces.api.dto.TokenResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservationFacade {
+
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    public ReservationFacade(UserService userService, TokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    /**
+     * 해당 유저의 잔액을 조회하기 위해 요청을 전달합니다.
+     * @param userId
+     * @return userId, balance
+     * @Author [kimseonkyoung]
+     */
+    public BalanceResponse getUserBalance(Long userId) {
+        // UserService 호출
+        UserServiceResponse response = userService.getUserBalance(userId);
+        return UserDtoConverter.toControllerResponse(response);
+    }
+
+    /**
+     * 해당 유저의 잔액을 충전하기 위해 요청을 전달합니다.
+     * @param  request
+     * @return 유저 충전 결과(UserServiceResponse)
+     * @Author [kimseonkyoung]
+     */
+    public BalanceResponse chargeUserBalance(BalanceRequest request) {
+        // Controller DTO -> Service DTO 변환
+        UserServiceRequest serviceRequest = UserDtoConverter.toServiceRequest(request);
+        // UserService 호출
+        UserServiceResponse response = userService.chargeUserBalance(serviceRequest);
+        return UserDtoConverter.toControllerResponse(response);
+    }
+
+    /**
+     * 해당 유저의 토큰을 발급하기 위해 요청을 전달합니다.
+     * @param  userId
+     * @return 토큰 반환(TokenResponse)
+     * @Author [kimseonkyoung]
+     */
+    public TokenResponse createToken(long userId) {
+        // TokenService 호출
+        TokenServiceResponse response = tokenService.createToken(userId);
+        return TokenDtoConverter.toControllerResponse(response);
+    }
 }
---
+++ src/test/java/kr/hhplus/be/server/application/FacadeMapperTest.java
@@ -0,0 +1,30 @@
+package kr.hhplus.be.server.application;
+
+import kr.hhplus.be.server.application.mapper.facade.UserDtoConverter;
+import kr.hhplus.be.server.domain.service.dto.UserServiceRequest;
+import kr.hhplus.be.server.interfaces.api.dto.BalanceRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class FacadeMapperTest {
+
+    private final UserDtoConverter userDtoConverter = new UserDtoConverter();
+
+    @Test
+    @DisplayName("controller 잔액 충전 DTO -> UserServiceResponse DTO로 전환: 해당 사항은 facade에서 변환")
+    void testToServiceDto() {
+        // given
+        BalanceRequest request = new BalanceRequest(1L, 1000);
+
+        // when
+        UserServiceRequest serviceRequest = userDtoConverter.toServiceRequest(request);
+
+        //then
+        assertNotNull(serviceRequest);
+        assertEquals(request.getUserId(), serviceRequest.getUserId());
+        assertEquals(request.getAmount(), serviceRequest.getAmount());
+    }
+}

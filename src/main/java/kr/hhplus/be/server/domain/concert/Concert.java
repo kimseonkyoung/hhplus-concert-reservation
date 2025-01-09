2e2e0f8d8ed3473f5740e132b368b95b8b35674e
Kimseonkyoung <kwswaver@gmail.com>
2025-01-10 06:05:52 +0900

    feat: 콘서트 도메인 관련 비즈니스 로직 추가
    - 콘서트 목록 조회
    - 콘서트 날짜 조회
    - 콘서트 예약 가능 좌석 조회

--- src/main/java/kr/hhplus/be/server/domain/concert/Concert.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/Concert.java
@@ -1,4 +1,34 @@
 package kr.hhplus.be.server.domain.concert;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "concert")
+@Setter
+@Getter
 public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "concert_id", nullable = false)
+    private Long concertId;
+
+    @Column(name = "concert_name", nullable = false)
+    private String title;
+
+    public Concert() {
+
+    }
+
+    private Concert(Long concertId, String title) {
+        this.concertId = concertId;
+        this.title = title;
+    }
+
+    // 생성자를 팩토리 메서드로 생성
+    public static Concert create(Long ConcertId, String title) {
+        return new Concert(ConcertId, title);
+    }
 }

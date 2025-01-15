81f3be5336d1ac2490dc16236f2b7d225b8442bc
Kimseonkyoung <kwswaver@gmail.com>
2025-01-16 00:50:44 +0900

    feat: 글로벌 예외처리를 위한 클래스 생성
    - global restControllerAdvice 생성
    - custom Exception 클래스 파일 생성
    - error code 정의를 위한 enum 파일 생성

---
+++ src/main/java/kr/hhplus/be/server/common/globalErrorHandler/ErrorResponse.java
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.common.globalErrorHandler;
+
+public class ErrorResponse {
+    private Integer code;
+    private String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getStatusCode(); // Enum 이름을 코드로 사용
+        this.message = errorCode.getMessage();
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/common/globalErrorHandler/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.common.globalErrorHandler;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.hhplus.be.server.domain.common.exception.InsufficientBalanceException;
+import kr.hhplus.be.server.domain.common.exception.ReservationNotFoundException;
+import kr.hhplus.be.server.domain.common.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static kr.hhplus.be.server.common.globalErrorHandler.ErrorCode.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handlerException(Exception e, HttpServletRequest request) {
+        return ResponseEntity.status(500).body(new kr.hhplus.be.server.common.globalErrorHandler.ErrorResponse(COMMON_ERROR));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(BAD_REQUEST));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> userNotFoundException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(MISSING_USER));
+    }
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ErrorResponse reservationNotFoundException() {
+        return new ErrorResponse(MISSING_RESERVATION);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ErrorResponse> InsufficientBalanceException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(INSUFFICIENT_BALANCE));
+    }
+}
+
+
